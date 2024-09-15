7781c4ac9266c52080ac0913b9bc8db47f96492f
Harsh Paliwal <paliwal.harsh08@gmail.com>
2024-09-15 21:42:27 +0530

    Add files via upload
    Code for while,do-while,for loop

---
+++ Whileloop.java
@@ -0,0 +1,24 @@
+public class Whileloop {
+    
+    public static void main(String[] args) {
+        int a=0;
+        while(a<5){
+            System.out.println(a);
+            a++;
+        }
+
+        int b = 10;
+        do{
+            System.out.println(b);
+            b++;
+        }while(b<5);
+
+        int i=0;
+
+        for(i=0;i<=10;i++){
+            System.out.println(i);
+
+        }
+
+    }
+}
